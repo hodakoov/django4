2677fb788599fd9d1c212843777a392c536ed59e
Hodakov <teeror93@gmail.com>
2023-08-01 16:06:33 +0400

    admin.py - новые правки и изменения в оторажении

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,4 +4,11 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_display = ['title', 'slug', 'author', 'publish', 'status'] # отображение в странице списка постов
+    list_filter = ['status', 'created', 'publish', 'author'] # боковая панель с фильтрацией
+    search_fields = ['title', 'body'] # Список поле по которым можно выполнять поиск в строке поиска
+    prepopulated_fields = {'slug': ('title',)} # теперь slug добавляется автоматически копируя текст с title
+    raw_id_fields = ['author'] # поле author отображается поисковым виджетом, а не выпадающим списком
+    date_hierarchy = 'publish' # ниже строки поиска отображаются навигационные ссылки для навигации по датам
+    ordering = ['status', 'publish'] # сортировка на странице списка постов
+
