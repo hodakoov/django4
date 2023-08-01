ce4404c24cfeb425b964c4365d8dcac87fe56670
Hodakov <teeror93@gmail.com>
2023-08-01 15:25:14 +0400

    settings.py - добавили приложение models.py - Создана модель Post. В ней несколько столбцов . ПЛюс сортировка

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+
+    class Status(models.TextChoices): # перечисляемый класс Status для выбора вариантов статуса поста
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True) # автоматическое добавление даты при создании
+    updated = models.DateTimeField(auto_now=True) # обновление даты автоматически при сохранении
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT) # статус постов
+
+    class Meta:
+        ordering = ['-publish'] # сортировка в БД Сначала последние посты
+        indexes = [models.Index(fields=['-publish'])] # позволяет определять в модели индексы базы данных
+
+    def __str__(self):
+        return self.title
