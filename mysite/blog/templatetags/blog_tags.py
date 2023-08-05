264629dced74c3ccf18c568cd9a0577f92f34113
Hodakov <teeror93@gmail.com>
2023-08-05 21:59:26 +0400

    Создание шаблонного тега для отображения постов с наибольшим числом комментариев

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from ..models import Post
 
 register = template.Library()
@@ -15,3 +17,10 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+# Отображать посты с наибольшим числом комментариев.
+@register.simple_tag  # простой шаблонный тег, который возвращает значение(я)
+def get_most_commented_posts(count=5):
+    # агрегируется общее число комментариев к каждому посту
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
