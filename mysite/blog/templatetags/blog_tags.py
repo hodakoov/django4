3b2cbdb0cbe4418d5abb5cd27b1a657a7bd34439
Hodakov <teeror93@gmail.com>
2023-08-05 21:41:48 +0400

    Создание шаблонного тега для отображения последних постов на боковой панели блога

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -4,6 +4,14 @@ from ..models import Post
 register = template.Library()
 
 
-@register.simple_tag
+# Получаем общее число опубликованных в блоге постов
+@register.simple_tag  # обрабатывает предоставленные данные и возвращает строковый литерал
 def total_posts():
     return Post.published.count()
+
+
+# Отображать последние посты на боковой панели блога
+@register.inclusion_tag('blog/post/latest_posts.html')  # -//- и возвращает прорисованный шаблон
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
