478438a7f813d1b28966e9081ec62c463f51377b
Hodakov <teeror93@gmail.com>
2023-08-06 19:13:59 +0400

    Установка postgresql. ( https://www.digitalocean.com/community/tutorials/how-to-set-up-django-with-postgres-nginx-and-gunicorn-on-ubuntu-18-04 ) 1. sudo apt install python3-pip python3-dev libpq-dev postgresql postgresql-contrib ЕСЛИ что то не получается, то удалить все sudo apt-get --purge remove postgresql\* и dpkg -l | grep postgres !!!Создание базы данных PostgreSQL и пользователя 1. sudo -u postgres psql 2. CREATE DATABASE blog; 3. CREATE USER blog_user WITH PASSWORD 'password'; 4. ALTER ROLE blog_user SET client_encoding TO 'utf8'; 5. ALTER ROLE blog_user SET default_transaction_isolation TO 'read committed'; 6. Вместо GRANT ALL PRIVILEGES ON DATABASE myproject TO blog_user; нужно вводить ALTER DATABASE blog OWNER TO blog_user; Выгрузить данные в формате JSON (делается до подключения PostgreSQL 1. python manage.py dumpdata --indent=2 --output=mysite_data.json Если не получиться, то: 1.1 python -Xutf8 manage.py dumpdata --indent=2 --output=mysite_data.json Загрузить данные в базу данных 1. python manage.py loaddata mysite_data.json

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('PSQL_NAME'),
+        'USER': os.getenv('PSQL_USER'),
+        'PASSWORD': os.getenv('PSQL_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
